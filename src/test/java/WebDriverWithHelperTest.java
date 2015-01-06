4aaad22a930657a9f4dbc6bdb7e8d5cb64a4e808
Faisal Humayun <faisal.humayun@outlook.com>
2015-01-06 20:35:33 +0000

    amendments to caps

--- src/test/java/WebDriverWithHelperTest.java
+++ src/test/java/WebDriverWithHelperTest.java
@@ -62,21 +62,14 @@ public class WebDriverWithHelperTest implements SauceOnDemandSessionIdProvider {
 	@Before
 	public void setUp() throws Exception {
 
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
-		desiredCapabilities.setVersion(System.getenv("SELENIUM_VERSION"));
-		desiredCapabilities.setCapability(CapabilityType.PLATFORM, System.getenv("SELENIUM_PLATFORM"));
-		WebDriver driver = new RemoteWebDriver(
-		            new URL(("http://" + authentication.getUsername() + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub"),
-		                desiredCapabilities));
-		/*DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("version", "17.0.1");
 		capabilities.setCapability("platform", Platform.XP);
 		capabilities.setCapability("name", this.getClass().getName() + "." + testName.getMethodName());
 		this.webDriver = new RemoteWebDriver(
 				new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
 				capabilities);
-		this.sessionId = ((RemoteWebDriver)webDriver).getSessionId().toString();*/
+		this.sessionId = ((RemoteWebDriver)webDriver).getSessionId().toString();
 
 	}
 
