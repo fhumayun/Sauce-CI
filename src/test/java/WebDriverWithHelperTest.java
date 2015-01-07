9cab7a08c02f8dad08eb156c2dd9555e5e3ffadf
Faisal Humayun <faisal.humayun@outlook.com>
2015-01-07 15:30:17 +0000

    Altered Caps

--- src/test/java/WebDriverWithHelperTest.java
+++ src/test/java/WebDriverWithHelperTest.java
@@ -5,6 +5,7 @@ import org.junit.*;
 import org.junit.rules.TestName;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
