8918af9278a13c2000b0dcdca390b86b1c913c72
Faisal Humayun <faisal.humayun@outlook.com>
2015-01-11 20:03:45 +0000

    Serialized QA Link Checker

--- src/test/java/WebDriverWithHelperTest.java
+++ src/test/java/WebDriverWithHelperTest.java
@@ -37,7 +37,7 @@ public class WebDriverWithHelperTest implements SauceOnDemandSessionIdProvider {
 	 * user name and access key. To use the authentication supplied by environment variables or
 	 * from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
 	 */
-	public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("fhumayun","a804f4e8-d94d-4872-805e-f28766906dca");
+	public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("userName","accessKey");
 
 	/**
 	 * JUnit Rule which marks Sauce Jobs as passed/failed when the test succeeds or fails.
