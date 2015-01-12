ebe4191086b904f557c9514761c427413b5b0534
Faisal Humayun <faisal.humayun@outlook.com>
2015-01-12 21:35:18 +0000

    mods

--- src/test/java/WebDriverWithHelperTest.java
+++ src/test/java/WebDriverWithHelperTest.java
@@ -5,16 +5,16 @@ import org.junit.*;
 import org.junit.rules.TestName;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
- * This WebDriverWithHelperTest runs Selenium tests with
+ * This WebDriverWithHelperTest test shows you how to run your Selenium tests with
  * <a href="http://saucelabs.com/ondemand">Sauce OnDemand</a>.
  *
  * This test uses {@link RemoteWebDriver} and also includes the <a href="https://github.com/saucelabs/sauce-java/tree/master/junit">Sauce JUnit</a>
@@ -27,39 +27,40 @@ import java.util.concurrent.TimeUnit;
  */
 public class WebDriverWithHelperTest implements SauceOnDemandSessionIdProvider {
 
-	private WebDriver webDriver;
-	private String sessionId;
+    private WebDriver webDriver;
+    private String sessionId;
 
-	/**
-	 * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied Sauce
-	 * user name and access key. To use the authentication supplied by environment variables or
-	 * from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
-	 */
-	public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("userName","accessKey");
+    /**
+     * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied Sauce
+     * user name and access key. To use the authentication supplied by environment variables or
+     * from an external file, use the no-arg {@link SauceOnDemandAuthentication} constructor.
+     */
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication("userName","accessKey");
+    
+    /**
+     * JUnit Rule which marks Sauce Jobs as passed/failed when the test succeeds or fails.
+     */
+    public @Rule
+    SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
 
-	/**
-	 * JUnit Rule which marks Sauce Jobs as passed/failed when the test succeeds or fails.
-	 */
-	public @Rule
-	SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
-
-	/**
-	 * JUnit Rule that records the test name of the current test. When this is referenced
-	 * during the creation of {@link DesiredCapabilities}, the test method name is assigned
-	 * to the Sauce Job name and recorded in Jenkins Console Output and in the Sauce Jobs
-	 * Report in the Jenkins project's home page.
-	 */
-	public @Rule TestName testName = new TestName();
+    /**
+     * JUnit Rule that records the test name of the current test. When this is referenced
+     * during the creation of {@link DesiredCapabilities}, the test method name is assigned
+     * to the Sauce Job name and recorded in Jenkins Console Output and in the Sauce Jobs
+     * Report in the Jenkins project's home page.
+     */
+    public @Rule TestName testName = new TestName();
 
 
-	/**
-	 * Creates a new {@link RemoteWebDriver} instance to be used to run WebDriver tests
-	 * using Sauce.
-	 *
-	 * @throws Exception thrown if an error occurs constructing the WebDriver
-	 */
-	@Before
-	public void setUp() throws Exception {
+    /**
+     * Creates a new {@link RemoteWebDriver} instance to be used to run WebDriver tests
+     * using Sauce.
+     *
+     * @throws Exception thrown if an error occurs constructing the WebDriver
+     */
+    @Before
+    public void setUp() throws Exception {
+
 		// Start with Jenkins
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 		desiredCapabilities.setBrowserName(System.getenv("SELENIUM_BROWSER"));
@@ -71,33 +72,31 @@ public class WebDriverWithHelperTest implements SauceOnDemandSessionIdProvider {
 		desiredCapabilities.setCapability("name", "HiC_QA_URL");
 		System.out.println("userName=" + userName);
 		System.out.println("accessKey=" + accessKey);
-		System.out.println("Run by" + Run_by);
-		
-		this.webDriver = new RemoteWebDriver(
-				new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
-				desiredCapabilities);				
-		this.sessionId = ((RemoteWebDriver)webDriver).getSessionId().toString();
+		System.out.println("TestedBy" + TestedBy);
+        this.webDriver = new RemoteWebDriver(
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+        this.sessionId = ((RemoteWebDriver)webDriver).getSessionId().toString();
 
-	}
+    }
 
-	@Test
-	public void validateTitle() throws Exception {
+    @Test
+    public void validateTitle() throws Exception {
 		String sessionId = ((RemoteWebDriver) webDriver).getSessionId().toString();
         System.out.println("SauceOnDemandSessionID=" + sessionId);
         String QA_Link = Utils.readPropertyOrEnv("QA_Link", "");
         webDriver.get(QA_Link);
-        //webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		webDriver.quit();
-	}
+    @After
+    public void tearDown() throws Exception {
+        webDriver.quit();
+    }
 
-	@Override
-	public String getSessionId() {
-		return sessionId;
-	}
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
 
 
 
